4a4f10f1ff6198d64841ff0af32672cf932b7c74
Homam <bluesnowball@gmail.com>
2018-02-05 16:06:37 +0100

    separated Subscription component from TableWithData

--- src/components/user_subscriptions/Table.js
+++ src/components/user_subscriptions/Table.js
@@ -1,35 +1,70 @@
 import React from 'react'
 import R from 'ramda'
 import stylus from './Table.styl'
+import type { FetchState } from '../../adts'
+import { match, fetchState } from '../../adts'
+import { debug } from 'util';
+import { setTimeout } from 'timers';
 
-export const ExportToExcel = ({onClick}) => {
+const fetchTransactions = rockman_id => new Promise((resolve, reject) =>
+  // fake!
+  setTimeout(() => resolve(R.range(0, 5).map(i => `Transaction ${i}`)), 2000)
+)
+
+export const ExportToExcel = ({ onClick }) => {
   return <div id='export-to-excel' onClick={onClick}>🗒 Export to Excel</div>
 }
 
-export const TableWithData = ({data}) => {
-  return (<div>
-            <p style={{ padding: '8px'}}>Number of Subscriptions : { data.length }</p>
-            { data.map((records, index) => (<div key={index}>
-              <table id='table'>
-                <tr>
-                  <th>
-                    STATUS
-                  </th>
-                  <td>
-                    { records.optout?  'INACTIVE' : 'ACTIVE' }
-                  </td>
-                </tr>
-                { R.keys(records).map((keys) =>
-                  <tr>
-                    <th>
-                      {keys.toUpperCase().replace(/_/g, ' ')}
-                    </th>
-                    <td>
-                      {records[keys] ? records[keys] : '-'}
-                    </td>
-                  </tr>)
-                    }
-              </table>
-            </div>))}
-  </div>)
+class Subscription extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      transactions: fetchState.Nothing()
+    }
+  }
+  render() {
+    const { record } = this.props
+    return <div>
+      <table id='table'>
+        <tr>
+          <th>
+            STATUS
+          </th>
+          <td>
+            {record.optout ? 'INACTIVE' : 'ACTIVE'}
+          </td>
+        </tr>
+        {R.keys(record).map((keys) =>
+          <tr>
+            <th>
+              {keys.toUpperCase().replace(/_/g, ' ')}
+            </th>
+            <td>
+              {record[keys] ? record[keys] : '-'}
+            </td>
+          </tr>)
+        }
+      </table>
+      {
+        match({
+          Nothing: () => <a style={{ color: 'blue', textDecoration: 'underline' }} onClick={() => {
+            this.setState({ transactions: fetchState.Loading() })
+            fetchTransactions(record['rockman_id'])
+              .then(ts => this.setState({ transactions: fetchState.Loaded(ts) }))
+              .catch(e => this.setState({ transactions: fetchState.Error(e) }))
+          }}>Fetch data for {record['rockman_id']}</a>
+          , Loading: () => 'LOADING...'
+          , Error: (error) => 'ERROR'
+          , Loaded: (transactions) => transactions.map((t, i) => <div key={i}>{t}</div>)
+        })(this.state.transactions)
+      }
+    </div>
+  }
 }
+
+export const TableWithData = ({ data }) => <div>
+  <p style={{ padding: '8px' }}>Number of Subscriptions : {data.length}</p>
+  {
+    data.map((record, index) => <Subscription key={index} record={record} />)
+  }
+</div>
