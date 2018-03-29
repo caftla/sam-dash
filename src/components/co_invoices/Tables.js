26a42928a057fe88058f976bcf5415bb92f65f72
Ahmed <ahmedalkuhlani@gmail.com>
2018-03-29 15:23:39 +0800

    changed text of co-invoice

--- src/components/co_invoices/Tables.js
+++ src/components/co_invoices/Tables.js
@@ -139,5 +139,5 @@ export const LetterBody = ({ name, email }) =>
     - Please note that according to Sam Media financial policy a payment is made once 500USD threshold is reached. <br />
     - Please prepare invoices to the two billing addresses indicated above accordingly. <br />
     - Kindly email the invoice to <span className="bolder-text"> { finance_email } </span>
-    and cc to <span className="bolder-text">{ email }</span>.<br /><br /><br />
+    and cc <span className="bolder-text">{ email }</span>.<br /><br /><br />
   </div>
