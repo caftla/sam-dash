a5cfafdcd96ee16003d92851439dfb594711b00e
Ahmed <ahmedalkuhlani@gmail.com>
2018-04-10 00:41:25 +0800

    co_invoices query rewrite

--- server/sql-templates/co_invoices/index.sql
+++ server/sql-templates/co_invoices/index.sql
@@ -1,31 +1,48 @@
+with Pixels as (
+    select 
+		  ub.country_code
+		, ub.operator_code
+    , sum(ub.pixel + ub.delayed_pixel) :: float as pixels
+		, sum(coalesce(ub.home_cpa, 0)) :: float as total
+    , c.home_cpa as cpa
+
+		from user_subscriptions ub
+		left join cpa c on c.cpa_id = ub.cpa_id
+		where ub.pixel_timestamp >= $[params.from_date_tz]$
+      and ub.pixel_timestamp < $[params.to_date_tz]$
+			and $[params.f_filter('ub', {fieldMap: {'publisher_id': 'pubid'}})]$
+			and ub.pixel_timestamp is not null
+			and c.home_cpa is not null
+
+			group by 1,2,5
+			order by 1,2,5 desc
+)
+
+, Views as (
+	select
+		us.country_code
+	, us.operator_code
+	, sum(coalesce(case when us.impression > 0 then 1 else 0 end, 0)) :: float as views
+  , sum(case when us.sale > 0 then 1 else 0 end) :: float as sales
+  , sum(case when us.resubscribe > 0 then 1 else 0 end) :: float as resubscribes
+	
+	from user_sessions us
+  where us.timestamp >= $[params.from_date_tz]$
+      and us.timestamp < $[params.to_date_tz]$
+			and $[params.f_filter('us', {fieldMap: {'publisher_id': 'pubid'}})]$
+
+			group by 1,2
+			order by 1,2 desc
+)
+
+
 select 
-    us.country_code
-  , us.operator_code
-  , c.home_cpa as cpa
-  , sum(coalesce(case when us.impression > 0 then 1 else 0 end, 0)) :: float as views
-  , sum(coalesce(case when
-      (ub.pixel > 0 or ub.delayed_pixel > 0)
-      and (ub.pixel_timestamp is not null)
-      and (ub.pixel_timestamp >= $[params.from_date_tz]$
-      and ub.pixel_timestamp < $[params.to_date_tz]$)
-      then 1 else 0 end, 0)) :: float as pixels  
-  , sum(case when ub.sale > 0 then 1 else 0 end) :: float as sales
-  , sum(case when ub.resubscribe > 0 then 1 else 0 end) :: float as resubscribes
-  , sum(coalesce(case when 
-      (c.home_cpa is not null) and 
-      (ub.pixel_timestamp >= $[params.from_date_tz]$
-      and ub.pixel_timestamp < $[params.to_date_tz]$) 
-      then c.home_cpa else 0 end, 0)) :: float as total
-  , co.timezone
+	p.*
+, v.*
+, co.timezone
 
-from user_sessions us
-left join countries co on co.country_code = us.country_code 
-left join user_subscriptions as ub on ub.rockman_id = us.rockman_id
-left join cpa c on c.cpa_id = ub.cpa_id
+from Pixels p
+		left join Views v on v.country_code = p.country_code and v.operator_code = p.operator_code
+		left join countries co on v.country_code = co.country_code
 
-where us.timestamp >=  $[params.from_date_tz]$
-  and us.timestamp <  $[params.to_date_tz]$
-  and $[params.f_filter('us', {fieldMap: {'publisher_id': 'pubid'}})]$
-
-group by us.country_code, us.operator_code, co.timezone, cpa
-order by us.country_code asc, total desc
+		order by 1,2
