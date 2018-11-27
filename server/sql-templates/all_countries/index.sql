6d75deed977b36db6a37651cbfd427078a642970
vladimir <vladimir@sam-media.com>
2018-11-27 13:51:53 +0100

    Removing events table usage, user_sessions and user_subscriptions connection optimization

--- server/sql-templates/all_countries/index.sql
+++ server/sql-templates/all_countries/index.sql
@@ -1,28 +1,25 @@
-with T as (
-  select
-      c.country_code
-    , c.operator_code 
-    , c.affiliate_id
-    , c.handle_name
-    , c.platform
-    , c.gateway
-    , c.ad_name
-    , c.scenario_name
-    , c.service_identifier1
-  
-  from events c
-  where c.timestamp >= '$from_date$'
-    and c.timestamp <= '$to_date$'
-  group by
-      c.country_code
-    , c.operator_code 
-    , c.affiliate_id
-    , c.handle_name
-    , c.platform
-    , c.gateway
-    , c.ad_name
-    , c.scenario_name
-    , c.service_identifier1
-) 
-select T.*, a.affiliate_name from T inner join affiliate_mapping a
-on a.affiliate_id = T.affiliate_id
+select
+  c.country_code
+  , c.operator_code
+  , c.affiliate_id
+  , c.handle_name
+  , c.platform
+  , c.gateway
+  , c.ad_name
+  , c.scenario_name
+  , c.service_identifier1
+  , c.affiliate_name
+from user_sessions c
+where c.timestamp >= '$from_date$'
+  and c.timestamp <= '$to_date$'
+group by
+  c.country_code
+  , c.operator_code
+  , c.affiliate_id
+  , c.handle_name
+  , c.platform
+  , c.gateway
+  , c.ad_name
+  , c.scenario_name
+  , c.service_identifier1
+  , c.affiliate_name
