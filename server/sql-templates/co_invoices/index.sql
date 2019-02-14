f49be4e5ecf4aede80384508bb7f6b331d737216
Ahmed <ahmedalkuhlani@gmail.com>
2019-02-14 11:36:58 +0800

    modified query to get cpa from user_subscriptions table

--- server/sql-templates/co_invoices/index.sql
+++ server/sql-templates/co_invoices/index.sql
@@ -3,16 +3,15 @@ with Pixels as (
 			ub.country_code
 		, ub.operator_code
 		, sum(case when ub.pixel > 0 or ub.delayed_pixel > 0 then 1 else 0 end) :: float as pixels
-    , sum(coalesce(c.home_cpa, 0)) :: float as total
-    , c.home_cpa as cpa
+    , sum(coalesce(ub.home_cpa, 0)) :: float as total
+    , ub.home_cpa as cpa
 
 		from user_subscriptions ub
-		left join cpa c on c.cpa_id = ub.cpa_id
 		where ub.pixel_timestamp >= $[params.from_date_tz]$
       and ub.pixel_timestamp < $[params.to_date_tz]$
 			and $[params.f_filter('ub', {fieldMap: {'publisher_id': 'pubid'}})]$
 			and ub.pixel_timestamp is not null
-			and c.home_cpa is not null
+			and ub.home_cpa is not null
 
 			group by 1,2,5
 			order by 1,2,5 desc
