014810af2e414191444780cc0e0e09a86edd0a6c
Emmancipate Musemwa <emman@sam-media.com>
2019-04-19 20:54:38 +0800

    fix toggle_show_link

--- src/components/ouisys_pages/campaigns/index.js
+++ src/components/ouisys_pages/campaigns/index.js
@@ -165,7 +165,7 @@ class ViewComponent extends Component {
             {
               (this.props.show_link_modal === true && this.props.created_campaign.hasOwnProperty("xcid")) &&
               <Modal
-                close={()=>this.props.toggleShowLink(false)}
+                close={()=>this.props.toggle_show_link(false)}
                 toggleShowLink={this.props.toggle_show_link}
                 created_campaign={this.props.created_campaign}
                 title="Campaign created successfully!"
